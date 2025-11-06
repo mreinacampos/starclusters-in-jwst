54ab4c30329fe1795c768507de9ccbbe9f0ba061
Marta Reina Campos <reina.campos@uni-heidelberg.de>
2025-11-06 10:29:42 -0500

    Updating the .gitignore file

--- build_website.sh
+++ build_website.sh
@@ -2,10 +2,10 @@
 
 # build the html files
 (cd web && bundle exec jekyll build)
-#cp -r _site ../
 rm -r ./docs
 mv ./web/_site/ ./docs
 git add docs
 git add web
+git add assets
 git commit -m "Updating the website - html and raw files"
 git push origin main
